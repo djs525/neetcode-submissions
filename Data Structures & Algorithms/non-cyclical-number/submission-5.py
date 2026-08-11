80471c6dd18ef6d60a6473da1a67358167920281
Dev Shah <djshah2903@gmail.com>
2026-08-11 10:14:17 -0400

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        def square(num):
+            ans = 0
+            while num > 0:
+                x = num % 10
+                ans += x*x
+                num = num // 10
+            return ans
+        
+        slow = square(n)
+        fast = square(square(n))
+
+        while slow != fast:
+            slow = square(slow)
+            fast = square(square(fast))
+
+            if slow == 1 or fast == 1:
+                return True
+        return slow == 1
