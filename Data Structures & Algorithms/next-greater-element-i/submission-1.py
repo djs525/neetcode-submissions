9ca3869c06255862331e29d166eb8b56377a1430
Dev Shah <djshah2903@gmail.com>
2026-08-13 15:21:05 -0400

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        stk = []
+
+        res = [-1] * len(nums1)
+        nums1Idx = {n:i for i,n in enumerate(nums1)} #storing nums: index for easy lookup
+
+        #traversing nums2
+        for i in range(len(nums2)):
+            cur = nums2[i]
+
+            while stk and cur > stk[-1]:
+                val = stk.pop()
+                res[nums1Idx[val]] = cur
+            
+            if cur in nums1Idx:
+                stk.append(cur)
+        return res
