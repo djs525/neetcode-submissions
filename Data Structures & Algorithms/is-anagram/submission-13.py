e0ba01e5c50f122b79c6b7e56f64fb043f21dbdb
Dev Shah <djshah2903@gmail.com>
2026-08-19 12:12:31 -0400

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) > len(t):
+            return False
+        
+        arr = [0] * 26
+
+        for c in s:
+            arr[ord(c) - ord('a')] += 1
+        
+        for c in t:
+            arr[ord(c) - ord('a')] -= 1
+        
+        if min(arr) < 0:
+            return False
+        else:
+            return True
